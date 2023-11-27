3436cfc98fe3671bbf3f4bc8da0b34169fdc0095
Chris Ziogas <ziogaschr@gmail.com>
2023-11-27 18:02:06 +0200

    Read express HOST from ENV vars

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const statsRouter = require('./routes/stats');
 const operatorsRouter = require('./routes/operators');
 
 const app = express();
+const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 
 // Enable CORS for all requests
@@ -20,6 +21,6 @@ app.use(statsRouter);
 app.use(operatorsRouter);
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Server running on port ${port}`);
 });
